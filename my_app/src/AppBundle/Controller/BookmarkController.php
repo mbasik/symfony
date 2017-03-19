cf786a6363c4957c22257986a6ead7cabb9182ea
Basia <bameliee@gmail.com>
2017-03-19 11:12:06 +0100

    nowe pliki z zajęć

---
+++ my_app/src/AppBundle/Controller/BookmarkController.php
@@ -0,0 +1,63 @@
+<?php
+/**
+ * Bookmark controller.
+ */
+
+namespace AppBundle\Controller;
+
+use AppBundle\Repository\BookmarkRepository;
+use Sensio\Bundle\FrameworkExtraBundle\Configuration\Route;
+use Sensio\Bundle\FrameworkExtraBundle\Configuration\Method;
+use Symfony\Bundle\FrameworkBundle\Controller\Controller;
+
+/**
+ * Class BookmarkController.
+ *
+ * @package AppBundle\Controller
+ *
+ * @Route("/bookmark")
+ */
+class BookmarkController extends Controller
+{
+    /**
+     * Index action.
+     *
+     * @return \Symfony\Component\HttpFoundation\Response Response
+     *
+     * @Route(
+     *     "/",
+     *     name="bookmark_index"
+     * )
+     */
+    public function indexAction()
+    {
+        $bookmarkRepository = new BookmarkRepository();
+        $bookmarks = $bookmarkRepository->findAll();
+
+        return $this->render(
+            'bookmark/index.html.twig',
+            ['bookmarks' => $bookmarks]
+        );
+    }
+    /**
+     * View action.
+     * @return \Symfony\Component\HttpFoundation\Response HTTP Response
+     *
+     * @Route(
+     *     "/{id}",
+     *     requirements={"id": "[1-9]\d*"},
+     *     name="bookmark_view",
+     * )
+     * @Method("GET")
+     */
+   public function viewAction($id)
+    {
+        $bookmarkRepository = new BookmarkRepository();
+        $bookmark = $bookmarkRepository->findOneById($id);
+        
+         return $this->render(
+            'bookmark/view.html.twig',
+            ['bookmark' => $bookmark]
+        );
+    }
+}
